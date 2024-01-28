4ce061d2ce93ce0a6bbf54ce2ac1e63f43f1a05d
a-belard <angebelard@gmail.com>
2024-01-28 20:18:04 +0200

    test

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,18 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
-const secretKey = process.env.JWT_SECRET || 'default-secret-key';
+const secretKey = process.env.JWT_SECRET || "default-secret-key";
 
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Access denied. Token missing.' });
+  const token = req.header("Authorization");
+  console.log({ token, headers: req.headers });
+  if (!token)
+    return res.status(401).json({ message: "Access denied. Token missing." });
 
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token.' });
-        req.user = user;
-        next();
-    });
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token." });
+    req.user = user;
+    next();
+  });
 };
 
 module.exports = authenticateToken;
