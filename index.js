82da9a70dc726159e3e5cbb36e2d6a563d6f937c
a-belard <angebelard@gmail.com>
2024-02-02 16:25:00 +0200

    update password

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "Practitioner@2024",
   database: "practitioner",
 });
 
