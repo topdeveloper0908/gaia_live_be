366d12420f404144e31a522ec973856aed830bb9
topdeveloper <topdeveloper0908@gmail.com>
2024-02-22 02:11:28 +0100

    update gmail

--- index.js
+++ index.js
@@ -414,8 +414,8 @@ app.post("/api/login_practitioner", async (req, res) => {
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "topdeveloper0908@gmail.com",
-        pass: "ycog yuqd qvla abha",
+        user: "jackchao1003@gmail.com",
+        pass: "xdst cmwc mpce byjo",
       },
     });
 
